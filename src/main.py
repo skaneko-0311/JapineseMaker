d1415f4a9b0030e8af9817623973832f5cba025c
skaneko-0311 <skaneko.0311@gmail.com>
2019-06-25 09:14:12 -0700

    Check some results and adjust the conditions

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@
 
 import sys
 import re
+import urllib.parse
+import urllib.request
+import json
 import MeCab
 
 
@@ -16,15 +19,35 @@ def parse_by_mecab(text):
     return tagger.parse(text)
 
 
+def transfer_to_kanji(word):
+    url = "http://www.google.com/transliterate?"
+    param = {"langpair": "ja-Hira|ja", "text": word}
+    param_encoded = urllib.parse.urlencode(param)
+    response = urllib.request.urlopen(url + param_encoded)
+    response_data = response.read()
+    parsed_data = json.loads(response_data)
+    fixed_data = json.loads(json.dumps(parsed_data[0], ensure_ascii=False))
+    return fixed_data[1][0]
+
+
 if __name__ == "__main__":
     text = get_text_from_args(sys.argv)
     parsed_text = parse_by_mecab(text)
     parsed_texts = parsed_text.split("\n")
-    elements = [re.split("\\s", item) for item in parsed_texts if item]
+    elements = [re.split("[\\s,]", item) for item in parsed_texts if item]
+    print(elements)
     japinese = []
     for element in elements:
         if element[0] == "EOS":
             continue
-        japinese_item = re.sub("[ぁ-んァ-ヴ]", "", element[0])
+        if element[1] == "名詞":
+            japinese_item = element[0]
+        elif element[1] == "助詞" or element[1] == "連体詞":
+            continue
+        elif re.match("[ぁ-んァ-ヴ]+", element[0]):
+            continue
+#            japinese_item = transfer_to_kanji(element[0])
+        else:
+            japinese_item = re.sub("[ぁ-んァ-ヴ]", "", element[0])
         japinese.append(japinese_item)
     print("".join(japinese))
